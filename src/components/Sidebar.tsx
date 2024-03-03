dd90cf7c76c7a2fdb64cf3df1a3807e510c412b2
RJ Nebrija <rj.nebrija@goabroad.com>
2024-03-03 14:20:33 +0800

    added navbar links bg changed when in current link

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,8 +2,15 @@
 import { CiViewList } from "react-icons/ci";
 import { BiCategory } from "react-icons/bi";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 export default function Sidebar() {
+  const locationURL = useLocation();
+  console.log("LOCATION");
+  console.log(locationURL);
+
+  const isCategories = locationURL.pathname.startsWith("/categories");
+
   return (
     <div className="bg-white hidden md:block rounded-lg min-w-[250px] max-h-[750px] h-fit ml-[-4px] py-6 ">
       <div className="flex gap-2">
@@ -14,16 +21,11 @@ export default function Sidebar() {
       </div>
 
       <div className="flex flex-col gap-4 mt-10">
-        {/* <div className="flex px-4 items-center  gap-6 rounded-lg mx-4 py-2 group hover:bg-purple-400 hover:cursor-pointer transition-colors">
-          <AiOutlineHome className="h-5 w-5 group-hover:text-white" />
-          <p className="text-black font-semibold group-hover:text-white">
-            Home
-          </p>
-        </div> */}
-
         <Link
           to="/"
-          className="flex px-4 items-center  gap-6 rounded-lg mx-4 group py-2 hover:bg-purple-400 hover:cursor-pointer transition-colors"
+          className={`flex px-4 items-center  gap-6 rounded-lg mx-4 group py-2 hover:bg-purple-400 hover:cursor-pointer transition-colors ${
+            !isCategories && "bg-purple-400 "
+          }`}
         >
           <CiViewList className="h-5 w-5 group-hover:text-white" />
           <p className="text-black font-semibold group-hover:text-white">
@@ -33,7 +35,9 @@ export default function Sidebar() {
 
         <Link
           to="/categories"
-          className="flex px-4 items-center  gap-6 rounded-lg mx-4 group py-2 hover:bg-purple-400 hover:cursor-pointer transition-colors"
+          className={`flex px-4 items-center  gap-6 rounded-lg mx-4 group py-2 hover:bg-purple-400 hover:cursor-pointer transition-colors ${
+            isCategories && "bg-purple-400 "
+          }`}
         >
           <BiCategory className="h-5 w-5 group-hover:text-white" />
           <p className="text-black font-semibold group-hover:text-white">
